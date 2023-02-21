abf110153e7e3aa6290cb497d1716664114883b1
nowakweronika <nowakweronika1301@gmail.com>
2023-02-21 12:20:46 +0100

    SIL-64: safe api call

--- app/src/main/java/com/appunite/loudius/network/ApiCallUtil.kt
+++ app/src/main/java/com/appunite/loudius/network/ApiCallUtil.kt
@@ -1,32 +1,19 @@
 package com.appunite.loudius.network
 
 import retrofit2.HttpException
-import retrofit2.Response
 import java.io.IOException
 
 suspend fun <T> safeApiCall(
     errorParser: RequestErrorParser = DefaultErrorParser,
-    apiCall: suspend () -> Response<T>
+    apiCall: suspend () -> T
 ): Result<T> {
     return try {
-        handleSuccessfulCall(apiCall, errorParser)
+        val response = apiCall()
+        Result.success(response)
     } catch (throwable: HttpException) {
-        Result.failure(WebException.UnknownError(throwable.code(), throwable.message()))
+        Result.failure(errorParser(throwable.code(), throwable.message()))
     } catch (throwable: IOException) {
         Result.failure(WebException.NetworkError(throwable))
-    }
-}
-
-private suspend fun <T> handleSuccessfulCall(
-    apiCall: suspend () -> Response<T>,
-    errorParser: RequestErrorParser
-): Result<T> {
-    val response = apiCall()
-    val body = response.body()
-    return if (response.isSuccessful && body != null) {
-        Result.success(body)
-    } else {
-        Result.failure(errorParser(response.code(), response.message()))
     }
 }
 
