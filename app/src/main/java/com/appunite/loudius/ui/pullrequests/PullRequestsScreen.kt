7e70cafdef5cc3dfa459431a9cb06c66d4ade558
Wojtek <wojciechkrystyniak@gmail.com>
2023-02-28 10:57:36 +0100

    Use lazy column

--- app/src/main/java/com/appunite/loudius/ui/pullrequests/PullRequestsScreen.kt
+++ app/src/main/java/com/appunite/loudius/ui/pullrequests/PullRequestsScreen.kt
@@ -2,13 +2,12 @@
 
 package com.appunite.loudius.ui.pullrequests
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -38,13 +37,12 @@ private fun PullRequestsScreenStateless(
             // TODO: navigation
         }
     }, content = { padding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .padding(padding)
                 .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
         ) {
-            pullRequests.forEach {
+            items(pullRequests) {
                 PullRequestItem(
                     repositoryName = it.fullRepositoryName,
                     pullRequestTitle = it.title,
