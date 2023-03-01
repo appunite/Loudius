1b2bb3e292a8ffe68529d6c5bde2c9589d5605e0
Krzysztof Daniluk <krzysztof.daniluk@appunite.com>
2023-03-01 12:42:50 +0100

    Merge data source functions to the existing GitHubPullRequestsDataSource.kt.

--- app/src/main/java/com/appunite/loudius/network/datasource/GitHubPullRequestsDataSource.kt
+++ app/src/main/java/com/appunite/loudius/network/datasource/GitHubPullRequestsDataSource.kt
@@ -1,17 +1,55 @@
 package com.appunite.loudius.network.datasource
 
 import com.appunite.loudius.network.model.PullRequestsResponse
+import com.appunite.loudius.network.model.Review
+import com.appunite.loudius.network.model.Reviewer
 import com.appunite.loudius.network.services.GithubPullRequestsService
 import com.appunite.loudius.network.utils.safeApiCall
 import javax.inject.Inject
 
+interface PullRequestDataSource {
+    suspend fun getReviewers(
+        owner: String,
+        repository: String,
+        pullRequestNumber: String,
+        accessToken: String
+    ): Result<List<Reviewer>>
+
+    suspend fun getReviews(
+        owner: String,
+        repository: String,
+        pullRequestNumber: String,
+        accessToken: String
+    ): Result<List<Review>>
+
+}
+
 const val auth_token = "BEARER xxxxxxx" // temporary solution
 
-class GitHubPullRequestsDataSource @Inject constructor(private val service: GithubPullRequestsService) {
+class GitHubPullRequestsDataSource @Inject constructor(private val service: GithubPullRequestsService) :
+    PullRequestDataSource {
     suspend fun getPullRequestsForUser(author: String): Result<PullRequestsResponse> = safeApiCall {
         service.getPullRequestsForUser(
             auth_token,
             "author:$author type:pr state:open",
         )
+    }
+
+    override suspend fun getReviewers(
+        owner: String,
+        repository: String,
+        pullRequestNumber: String,
+        accessToken: String
+    ): Result<List<Reviewer>> = safeApiCall {
+        service.getReviewers(owner, repository, pullRequestNumber, accessToken)
+    }
+
+    override suspend fun getReviews(
+        owner: String,
+        repository: String,
+        pullRequestNumber: String,
+        accessToken: String
+    ): Result<List<Review>> = safeApiCall {
+        service.getReviews(owner, repository, pullRequestNumber, accessToken)
     }
 }
