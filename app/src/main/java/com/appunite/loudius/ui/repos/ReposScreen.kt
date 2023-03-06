e372bb5acd0429434c3efc69430a8f9e242a1d07
Krzysztof Daniluk <krzysztof.daniluk@appunite.com>
2023-03-06 15:51:01 +0100

    Add Launched effect for getting access token.

--- app/src/main/java/com/appunite/loudius/ui/repos/ReposScreen.kt
+++ app/src/main/java/com/appunite/loudius/ui/repos/ReposScreen.kt
@@ -1,9 +1,13 @@
 package com.appunite.loudius.ui.repos
 
 import android.content.Intent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.hilt.navigation.compose.hiltViewModel
+
 
 @Composable
 fun ReposScreen(
@@ -11,8 +15,14 @@ fun ReposScreen(
     viewModel: ReposViewModel = hiltViewModel(),
 ) {
     val code = intent.data?.getQueryParameter("code")
-    Text(text = code ?: "empty code")
-    code?.let {
-        viewModel.getAccessToken(code)
+    val rememberedCode = rememberUpdatedState(newValue = code)
+    Column {
+        Text(text = code ?: "code is already consumed")
+    }
+    LaunchedEffect(key1 = rememberedCode) {
+        rememberedCode.value?.let {
+            viewModel.getAccessToken(it)
+            intent.data = null
+        }
     }
 }
