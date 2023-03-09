521ec73b90585a585ba98e2423b88c64633ab946
Krzysztof Daniluk <krzysztof.daniluk@appunite.com>
2023-03-09 09:15:26 +0100

    Refactor ReviewersViewModel.kt code.

--- app/src/main/java/com/appunite/loudius/ui/reviewers/ReviewersViewModel.kt
+++ app/src/main/java/com/appunite/loudius/ui/reviewers/ReviewersViewModel.kt
@@ -7,11 +7,13 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.appunite.loudius.common.Screen
-import com.appunite.loudius.domain.GitHubPullRequestsRepository
+import com.appunite.loudius.domain.PullRequestsRepository
 import com.appunite.loudius.domain.model.Reviewer
+import com.appunite.loudius.network.model.RequestedReviewersResponse
+import com.appunite.loudius.network.model.Review
 import dagger.hilt.android.lifecycle.HiltViewModel
-import java.time.Duration
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 
@@ -22,7 +24,7 @@ data class ReviewersState(
 
 @HiltViewModel
 class ReviewersViewModel @Inject constructor(
-    private val repository: GitHubPullRequestsRepository,
+    private val repository: PullRequestsRepository,
     savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
@@ -40,51 +42,56 @@ class ReviewersViewModel @Inject constructor(
         }
     }
 
-    private fun getInitialValues(savedStateHandle: SavedStateHandle): InitialValues {
-        val owner: String = checkNotNull(savedStateHandle[Screen.Reviewers.ownerArg])
-        val repo: String = checkNotNull(savedStateHandle[Screen.Reviewers.repoArg])
-        val pullRequestNumber: String =
-            checkNotNull(savedStateHandle[Screen.Reviewers.pullRequestNumberArg])
-        val submissionDate: String =
-            checkNotNull(savedStateHandle[Screen.Reviewers.submissionDateArg])
-        return InitialValues(owner, repo, pullRequestNumber, submissionDate)
-    }
+    private fun getInitialValues(savedStateHandle: SavedStateHandle) = InitialValues(
+        checkNotNull(savedStateHandle[Screen.Reviewers.ownerArg]),
+        checkNotNull(savedStateHandle[Screen.Reviewers.repoArg]),
+        checkNotNull(savedStateHandle[Screen.Reviewers.pullRequestNumberArg]),
+        checkNotNull(savedStateHandle[Screen.Reviewers.submissionDateArg])
+    )
 
-    private suspend fun fetchRequestedReviewers(
-        initialValues: InitialValues
-    ) {
+
+    private suspend fun fetchRequestedReviewers(initialValues: InitialValues) {
         val (owner, repo, pullRequestNumber, submissionTime) = initialValues
         val hoursFromPRStart = countHoursTillNow(LocalDateTime.parse(submissionTime))
-        repository.getReviewers(owner, repo, pullRequestNumber).onSuccess { response ->
-            val reviewers = response.users.map {
-                Reviewer(it.id, it.login, false, hoursFromPRStart, null)
+
+        repository.getReviewers(owner, repo, pullRequestNumber)
+            .onSuccess { response ->
+                val reviewers = response.mapToReviewers(hoursFromPRStart)
+                state = state.copy(reviewers = state.reviewers + reviewers)
             }
-            state = state.copy(reviewers = state.reviewers + reviewers)
-        }
+    }
+
+    private fun RequestedReviewersResponse.mapToReviewers(hoursFromPRStart: Long) = users.map {
+        Reviewer(it.id, it.login, false, hoursFromPRStart, null)
     }
 
     private suspend fun fetchReviews(initialValues: InitialValues) {
         val (owner, repo, pullRequestNumber, submissionTime) = initialValues
         val hoursFromPRStart = countHoursTillNow(LocalDateTime.parse(submissionTime))
-        repository.getReviews(owner, repo, pullRequestNumber).onSuccess { reviews ->
-            reviews.groupBy { it.user.id }.map { singleUser ->
-                val latestReview = singleUser.value.minBy { it.submittedAt }
-                val hoursFromReviewDone = countHoursTillNow(latestReview.submittedAt)
 
-                Reviewer(
-                    latestReview.user.id,
-                    latestReview.user.login,
-                    true,
-                    hoursFromPRStart,
-                    hoursFromReviewDone
-                )
-            }
+        repository.getReviews(owner, repo, pullRequestNumber).onSuccess { reviews ->
+            val reviewersAfterReview = reviews.mapToReviewers(hoursFromPRStart)
+            state = state.copy(reviewers = state.reviewers + reviewersAfterReview)
+
         }
     }
 
-    private fun countHoursTillNow(submissionTime: LocalDateTime): Long =
-        Duration.between(submissionTime, LocalDateTime.now()).toHours()
+    private fun List<Review>.mapToReviewers(hoursFromPRStart: Long) = groupBy { it.user.id }
+        .map { reviewsForSingleUser ->
+            val latestReview = reviewsForSingleUser.value.minBy { it.submittedAt }
+            val hoursFromReviewDone = countHoursTillNow(latestReview.submittedAt)
 
+            Reviewer(
+                latestReview.user.id,
+                latestReview.user.login,
+                true,
+                hoursFromPRStart,
+                hoursFromReviewDone
+            )
+        }
+
+    private fun countHoursTillNow(submissionTime: LocalDateTime): Long =
+        ChronoUnit.HOURS.between(submissionTime, LocalDateTime.now())
 
     private data class InitialValues(
         val owner: String,
