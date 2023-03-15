dfdfa21af18b51399490d164795894001d00b636
nowakweronika <nowakweronika1301@gmail.com>
2023-03-15 15:17:49 +0100

    removal of given, when, then comments

--- app/src/test/java/com/appunite/loudius/ui/loading/LoadingViewModelTest.kt
+++ app/src/test/java/com/appunite/loudius/ui/loading/LoadingViewModelTest.kt
@@ -25,10 +25,8 @@ class LoadingViewModelTest {
 
     @Test
     fun `GIVEN valid code WHEN setCodeAndGetAccessToken THEN set code, access token and navigateToPullRequests`() {
-        // when
         viewModel.setCodeAndGetAccessToken(EXAMPLE_CODE)
 
-        // then
         assertEquals(viewModel.state.code, EXAMPLE_CODE)
         assertEquals(viewModel.state.accessToken, EXAMPLE_ACCESS_TOKEN)
         assertEquals(viewModel.state.navigateToPullRequests, NavigateToPullRequests)
@@ -36,28 +34,22 @@ class LoadingViewModelTest {
 
     @Test
     fun `GIVEN OnTryAgain action WHEN onAction THEN set showErrorScreen and get access token`() {
-        // given
         val action = LoadingAction.OnTryAgainClick
         viewModel.setCodeAndGetAccessToken(EXAMPLE_CODE)
 
-        // when
         viewModel.onAction(action)
 
-        // then
         assertEquals(viewModel.state.showErrorScreen, false)
         assertEquals(viewModel.state.accessToken, EXAMPLE_ACCESS_TOKEN)
     }
 
     @Test
     fun `GIVEN OnNavigateToPullRequests action WHEN onAction THEN set navigateToPullRequests as null`() {
-        // given
         val action = LoadingAction.OnNavigateToPullRequests
         viewModel.setCodeAndGetAccessToken(EXAMPLE_CODE)
 
-        // when
         viewModel.onAction(action)
 
-        // then
         assertEquals(viewModel.state.navigateToPullRequests, null)
     }
 }
