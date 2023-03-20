1ec2ea7c85e3113b8fe1cdb6280080948287d2cd
kezc <kezc@users.noreply.github.com>
2023-03-20 08:09:17 +0000

    [MegaLinter] Apply linters fixes

--- app/src/main/java/com/appunite/loudius/ui/pullrequests/PullRequestsScreen.kt
+++ app/src/main/java/com/appunite/loudius/ui/pullrequests/PullRequestsScreen.kt
@@ -5,7 +5,6 @@ package com.appunite.loudius.ui.pullrequests
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -22,7 +20,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
