0c387c8a43966989cd22e3416881eb85522a8bc1
Krzysiudan <Krzysiudan@users.noreply.github.com>
2023-03-27 11:00:38 +0000

    [MegaLinter] Apply linters fixes

--- app/src/main/java/com/appunite/loudius/di/ServiceModule.kt
+++ app/src/main/java/com/appunite/loudius/di/ServiceModule.kt
@@ -7,8 +7,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
