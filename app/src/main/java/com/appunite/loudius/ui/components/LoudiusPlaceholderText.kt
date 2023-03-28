a42374ac41e2d7cc0c013734092def9f15713a60
kezc <kezc@users.noreply.github.com>
2023-03-28 10:58:44 +0000

    [MegaLinter] Apply linters fixes

--- app/src/main/java/com/appunite/loudius/ui/components/LoudiusPlaceholderText.kt
+++ app/src/main/java/com/appunite/loudius/ui/components/LoudiusPlaceholderText.kt
@@ -1,7 +1,6 @@
 package com.appunite.loudius.ui.components
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +9,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,11 +23,11 @@ fun LoudiusPlaceholderText(@StringRes textId: Int, padding: PaddingValues) {
             .padding(padding)
             .fillMaxSize()
             .padding(16.dp),
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.Center,
     ) {
         Text(
             text = stringResource(id = textId),
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
         )
     }
 }
--- app/src/main/java/com/appunite/loudius/ui/reviewers/ReviewersScreen.kt
+++ app/src/main/java/com/appunite/loudius/ui/reviewers/ReviewersScreen.kt
@@ -279,6 +279,7 @@ fun DetailsScreenPreview() {
         )
     }
 }
+
 @Preview
 @Composable
 fun DetailsScreenNoReviewsPreview() {
