9ac1ed249d0bfb833530180ace22782a69b54d5a
Wojtek <wojciechkrystyniak@gmail.com>
2023-04-18 10:51:14 +0200

    Clean up test and gradle

--- app/src/androidTest/java/com/appunite/loudius/LoginScreenTest.kt
+++ app/src/androidTest/java/com/appunite/loudius/LoginScreenTest.kt
@@ -19,17 +19,13 @@ package com.appunite.loudius
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.appunite.loudius.ui.login.LoginScreen
 import com.appunite.loudius.ui.theme.LoudiusTheme
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
 class LoginScreenTest {
 
@@ -38,11 +34,6 @@ class LoginScreenTest {
 
     @get:Rule(order = 1)
     val composeTestRule = createAndroidComposeRule<TestActivity>()
-
-    @Before
-    fun setUp() {
-        hiltRule.inject()
-    }
 
     @Test
     fun whenTheLoginScreenIsVisibleThenTheLogInButtonIsVisible() {
