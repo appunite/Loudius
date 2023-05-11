56ad1521d69dd9e3f05bdda784c40f66b16111cd
jacek-marchwicki <jacek-marchwicki@users.noreply.github.com>
2023-05-11 12:29:51 +0000

    [MegaLinter] Apply linters fixes

--- app/src/main/java/com/appunite/loudius/ui/components/LoudiusFullScreenError.kt
+++ app/src/main/java/com/appunite/loudius/ui/components/LoudiusFullScreenError.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.appunite.loudius.R
 import com.appunite.loudius.ui.components.utils.MultiScreenPreviews
