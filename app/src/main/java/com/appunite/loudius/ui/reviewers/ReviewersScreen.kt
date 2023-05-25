9c00280c5e69041930a39a6b2200ee22aebe2573
nowakweronika <nowakweronika@users.noreply.github.com>
2023-05-25 14:51:22 +0000

    [MegaLinter] Apply linters fixes

--- app/src/main/java/com/appunite/loudius/ui/reviewers/ReviewersScreen.kt
+++ app/src/main/java/com/appunite/loudius/ui/reviewers/ReviewersScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.PullRefreshState
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
