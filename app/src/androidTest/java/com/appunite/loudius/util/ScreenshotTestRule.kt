ecb77642fb8a7d67536717b6200a03cdbac851e9
nowakweronika <nowakweronika1301@gmail.com>
2023-06-12 09:01:39 +0200

    removal of tryCount value

--- app/src/androidTest/java/com/appunite/loudius/util/ScreenshotTestRule.kt
+++ app/src/androidTest/java/com/appunite/loudius/util/ScreenshotTestRule.kt
@@ -26,29 +26,17 @@ import org.junit.runners.model.Statement
 import java.util.concurrent.atomic.AtomicBoolean
 
 /**
- * TestRule used to run all test methods try count 1 time. Take screenshots on failure.
+ * TestRule used to take screenshot on failure.
  */
 open class ScreenshotTestRule : TestRule {
-    // Note: Data seeding must happen before we run a test. As a result, retrying failed tests
-    //       at the JUnit level doesn"t make sense because we can"t run data seeeding.
-    //
-    // Run all test methods tryCount times. Take screenshots on failure.
-    // A method rule would allow targeting specific (method.getAnnotation(Retry.class))
-    private val tryCount = 1
 
     override fun apply(base: Statement, description: Description): Statement {
         return object : Statement() {
             @Throws(Throwable::class)
             override fun evaluate() {
                 var error: Throwable? = null
-
                 val errorHandled = AtomicBoolean(false)
 
-                // Espresso failure handler will capture accurate UI screenshots.
-                // if we wait for `try { base.evaluate() } catch ()` then the UI will be in a different state
-                //
-                // Only espresso failures trigger the espresso failure handlers. For JUnit assert errors,
-                // those must be captured in `try { base.evaluate() } catch ()`
                 Espresso.setFailureHandler { throwable, matcher ->
                     Screenshot.capture().process()
                     errorHandled.set(true)
@@ -56,17 +44,15 @@ open class ScreenshotTestRule : TestRule {
                     DefaultFailureHandler(targetContext).handle(throwable, matcher)
                 }
 
-                for (i in 0 until tryCount) {
-                    errorHandled.set(false)
-                    try {
-                        base.evaluate()
-                        return
-                    } catch (t: Throwable) {
-                        if (!errorHandled.get()) {
-                            Screenshot.capture().process()
-                        }
-                        error = t
+                errorHandled.set(false)
+                try {
+                    base.evaluate()
+                    return
+                } catch (t: Throwable) {
+                    if (!errorHandled.get()) {
+                        Screenshot.capture().process()
                     }
+                    error = t
                 }
 
                 if (error != null) throw error
