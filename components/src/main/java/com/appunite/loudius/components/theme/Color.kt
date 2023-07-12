0ef36f57ca05c72d4fdeb91510986d68a5da3fe5
Krzysztof Daniluk <krzysztof.daniluk@appunite.com>
2023-07-12 13:25:00 +0200

    Add colours to the Showkase component browser.

--- components/src/main/java/com/appunite/loudius/components/theme/Color.kt
+++ components/src/main/java/com/appunite/loudius/components/theme/Color.kt
@@ -17,18 +17,37 @@
 package com.appunite.loudius.components.theme
 
 import androidx.compose.ui.graphics.Color
+import com.airbnb.android.showkase.annotation.ShowkaseColor
 
+@ShowkaseColor(name = "primary", group = "Dark mode")
 val Purple80 = Color(0xFFD0BCFF)
+
+@ShowkaseColor(name = "secondary", group = "Dark mode")
 val PurpleGrey80 = Color(0xFFCCC2DC)
+
+@ShowkaseColor(name = "tertiary", group = "Dark mode")
 val Pink80 = Color(0xFFEFB8C8)
 
+@ShowkaseColor(name = "primary", group = "Light mode")
 val Purple40 = Color(0xFF6650a4)
+
+@ShowkaseColor(name = "secondary", group = "Light mode")
 val PurpleGrey40 = Color(0xFF625b71)
+
+@ShowkaseColor(name = "tertiary", group = "Light mode")
 val Pink40 = Color(0xFF7D5260)
 
+@ShowkaseColor(name = "surface", group = "Light mode")
 val White99 = Color(0xFFFFFBFE)
+
+@ShowkaseColor(name = "onSurface", group = "Light mode")
 val Black90 = Color(0xFF1C1B1F)
 
+@ShowkaseColor(name = "onSurfaceVariant", group = "Light mode")
 val NeutralVariant30 = Color(0xFF49454F)
+
+@ShowkaseColor(name = "outlineVariant", group = "Light mode")
 val NeutralVariant80 = Color(0xFFCAC4D0)
+
+@ShowkaseColor(name = "error", group = "Light mode")
 val Error20 = Color(0xFF601410)
