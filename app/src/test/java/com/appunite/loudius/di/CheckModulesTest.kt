0786d3afed0e6a30fbb6c09294d5bba1f67e9f54
Mateusz <m.pachulski94@gmail.com>
2023-09-22 16:39:41 +0200

    exclude viewmodel from test modules

--- app/src/test/java/com/appunite/loudius/di/CheckModulesTest.kt
+++ app/src/test/java/com/appunite/loudius/di/CheckModulesTest.kt
@@ -2,19 +2,25 @@ package com.appunite.loudius.di
 
 
 import android.content.Context
+import android.content.SharedPreferences
+import androidx.lifecycle.SavedStateHandle
+import com.appunite.loudius.util.MainDispatcherExtension
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkClass
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.koin.dsl.koinApplication
 import org.koin.test.KoinTest
 import org.koin.test.check.checkModules
 import org.koin.test.mock.MockProvider
 
+@ExtendWith(MainDispatcherExtension::class)
 class CheckModulesTest : KoinTest {
 
     @Test
     fun verifyKoinApp() {
-        MockProvider.register{mockkClass(it)}
+        MockProvider.register { mockkClass(it) }
 
         koinApplication {
             modules(
@@ -25,8 +31,8 @@ class CheckModulesTest : KoinTest {
                 repositoryModule,
                 serviceModule
             )
+
             checkModules() {
-                withInstance<Context>()
             }
         }
     }
