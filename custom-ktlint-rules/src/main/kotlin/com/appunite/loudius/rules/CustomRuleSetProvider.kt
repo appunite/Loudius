f4e17fe5bd9eb94f26a69576fc752fab8058c8d7
Mateusz <m.pachulski94@gmail.com>
2023-10-03 15:07:08 +0200

    import rules from twitter

--- custom-ktlint-rules/src/main/kotlin/com/appunite/loudius/rules/CustomRuleSetProvider.kt
+++ custom-ktlint-rules/src/main/kotlin/com/appunite/loudius/rules/CustomRuleSetProvider.kt
@@ -2,13 +2,46 @@ package com.appunite.loudius.rules
 
 import com.pinterest.ktlint.core.RuleProvider
 import com.pinterest.ktlint.core.RuleSetProviderV2
-import com.twitter.compose.rules.ktlint.TwitterComposeRuleSetProvider
+import com.twitter.compose.rules.ktlint.ComposeCompositionLocalAllowlistCheck
+import com.twitter.compose.rules.ktlint.ComposeCompositionLocalNamingCheck
+import com.twitter.compose.rules.ktlint.ComposeContentEmitterReturningValuesCheck
+import com.twitter.compose.rules.ktlint.ComposeModifierComposableCheck
+import com.twitter.compose.rules.ktlint.ComposeModifierMissingCheck
+import com.twitter.compose.rules.ktlint.ComposeModifierReusedCheck
+import com.twitter.compose.rules.ktlint.ComposeModifierWithoutDefaultCheck
+import com.twitter.compose.rules.ktlint.ComposeMultipleContentEmittersCheck
+import com.twitter.compose.rules.ktlint.ComposeMutableParametersCheck
+import com.twitter.compose.rules.ktlint.ComposeNamingCheck
+import com.twitter.compose.rules.ktlint.ComposeParameterOrderCheck
+import com.twitter.compose.rules.ktlint.ComposePreviewNamingCheck
+import com.twitter.compose.rules.ktlint.ComposePreviewPublicCheck
+import com.twitter.compose.rules.ktlint.ComposeRememberMissingCheck
+import com.twitter.compose.rules.ktlint.ComposeUnstableCollectionsCheck
+import com.twitter.compose.rules.ktlint.ComposeViewModelForwardingCheck
+import com.twitter.compose.rules.ktlint.ComposeViewModelInjectionCheck
 
 internal const val RULE_SET_ID = "loudius-rule-set-id"
 
 class CustomRuleSetProvider : RuleSetProviderV2(id = RULE_SET_ID, about = NO_ABOUT) {
     override fun getRuleProviders(): Set<RuleProvider> =
         setOf(
-            RuleProvider { UseStriktAssertionLibrary() }
+            RuleProvider { UseStriktAssertionLibrary() },
+            RuleProvider { ComposeCompositionLocalAllowlistCheck() },
+            RuleProvider { ComposeCompositionLocalNamingCheck() },
+            RuleProvider { ComposeContentEmitterReturningValuesCheck() },
+            RuleProvider { ComposeModifierComposableCheck() },
+            RuleProvider { ComposeModifierMissingCheck() },
+            RuleProvider { ComposeModifierReusedCheck() },
+            RuleProvider { ComposeModifierWithoutDefaultCheck() },
+            RuleProvider { ComposeMultipleContentEmittersCheck() },
+            RuleProvider { ComposeMutableParametersCheck() },
+            RuleProvider { ComposeNamingCheck() },
+            RuleProvider { ComposeParameterOrderCheck() },
+            RuleProvider { ComposePreviewNamingCheck() },
+            RuleProvider { ComposePreviewPublicCheck() },
+            RuleProvider { ComposeRememberMissingCheck() },
+            RuleProvider { ComposeUnstableCollectionsCheck() },
+            RuleProvider { ComposeViewModelForwardingCheck() },
+            RuleProvider { ComposeViewModelInjectionCheck() }
         )
 }
