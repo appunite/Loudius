083868b927174a6d3089582e643c53096878cba1
Mateusz <m.pachulski94@gmail.com>
2023-10-04 16:18:06 +0200

    remove imports

--- custom-ktlint-rules/src/main/kotlin/com/appunite/loudius/rules/CustomRuleSetProvider.kt
+++ custom-ktlint-rules/src/main/kotlin/com/appunite/loudius/rules/CustomRuleSetProvider.kt
@@ -2,23 +2,6 @@ package com.appunite.loudius.rules
 
 import com.pinterest.ktlint.core.RuleProvider
 import com.pinterest.ktlint.core.RuleSetProviderV2
-import com.twitter.compose.rules.ktlint.ComposeCompositionLocalAllowlistCheck
-import com.twitter.compose.rules.ktlint.ComposeCompositionLocalNamingCheck
-import com.twitter.compose.rules.ktlint.ComposeContentEmitterReturningValuesCheck
-import com.twitter.compose.rules.ktlint.ComposeModifierComposableCheck
-import com.twitter.compose.rules.ktlint.ComposeModifierMissingCheck
-import com.twitter.compose.rules.ktlint.ComposeModifierReusedCheck
-import com.twitter.compose.rules.ktlint.ComposeModifierWithoutDefaultCheck
-import com.twitter.compose.rules.ktlint.ComposeMultipleContentEmittersCheck
-import com.twitter.compose.rules.ktlint.ComposeMutableParametersCheck
-import com.twitter.compose.rules.ktlint.ComposeNamingCheck
-import com.twitter.compose.rules.ktlint.ComposeParameterOrderCheck
-import com.twitter.compose.rules.ktlint.ComposePreviewNamingCheck
-import com.twitter.compose.rules.ktlint.ComposePreviewPublicCheck
-import com.twitter.compose.rules.ktlint.ComposeRememberMissingCheck
-import com.twitter.compose.rules.ktlint.ComposeUnstableCollectionsCheck
-import com.twitter.compose.rules.ktlint.ComposeViewModelForwardingCheck
-import com.twitter.compose.rules.ktlint.ComposeViewModelInjectionCheck
 
 internal const val RULE_SET_ID = "loudius-rule-set-id"
 
