f2ae0315c4e3d88beb59da25f923b4f348dfc87d
Jacek Marchwicki <jacek.marchwicki@gmail.com>
2023-10-12 10:06:09 +0200

    chore: ktlint for app-shared-tests

--- app-shared-tests/src/test/java/com/appunite/loudius/util/MockWebServerRuleTest.kt
+++ app-shared-tests/src/test/java/com/appunite/loudius/util/MockWebServerRuleTest.kt
@@ -114,7 +114,7 @@ class MockWebServerRuleTest {
                     .contains("An test exception occurred, but we also found some not mocked requests")
                 expectThat(it).isA<MultipleFailuresError>().get(MultipleFailuresError::failures)
                     .first().isA<CustomException>()
-            },
+            }
         )
 
         executeRequest("https://example.com/not_mocked")
@@ -139,7 +139,7 @@ class MockWebServerRuleTest {
                             .contains("Request: GET https://example.com/not_mocked, there are no mocks")
                         get(MultipleFailuresError::failures).isEmpty()
                     }
-            },
+            }
         )
 
         executeRequest("https://example.com/not_mocked")
@@ -177,7 +177,7 @@ class MockWebServerRuleTest {
                                 message.isNotNull().contains("found \"/not_mocked\"")
                             }
                     }
-            },
+            }
         )
 
         executeRequest("https://example.com/not_mocked")
@@ -233,7 +233,7 @@ class MockWebServerRuleTest {
         val slot = executeRequestAndGetMockedArgumentRequest {
             it.method(
                 "POST",
-                "Request body".toRequestBody(),
+                "Request body".toRequestBody()
             )
         }
 
@@ -248,7 +248,7 @@ class MockWebServerRuleTest {
         expectedException.expect(
             matcher<Any> {
                 expectThat(it).isA<CustomException>()
-            },
+            }
         )
 
         throw CustomException
@@ -256,21 +256,21 @@ class MockWebServerRuleTest {
 
     private fun executeRequest(
         url: String,
-        builder: (okhttp3.Request.Builder) -> okhttp3.Request.Builder = { it },
+        builder: (okhttp3.Request.Builder) -> okhttp3.Request.Builder = { it }
     ): Response {
         val client = OkHttpClient.Builder()
             .addInterceptor(TestInterceptor)
             .build()
         val request = builder(
             okhttp3.Request.Builder()
-                .url(url),
+                .url(url)
         )
             .build()
         return client.newCall(request).execute()
     }
 
     private fun executeRequestAndGetMockedArgumentRequest(
-        requestBuilder: (okhttp3.Request.Builder) -> okhttp3.Request.Builder = { it },
+        requestBuilder: (okhttp3.Request.Builder) -> okhttp3.Request.Builder = { it }
     ): CapturingSlot<Request> {
         val slot = slot<Request>()
         val mock = mockk<ResponseGenerator> {
@@ -311,10 +311,10 @@ private fun <T> matcher(check: (T) -> Unit): Matcher<T> = object : TypeSafeMatch
 class LoggerMockRule : TestWatcher() {
     override fun starting(description: org.junit.runner.Description?) {
         mockkStatic(Log::class)
-        every { Log.v(any(), any())} returns 0
-        every { Log.v(any(), any(), any())} returns 0
-        every { Log.w(any(), any<String>())} returns 0
-        every { Log.w(any(), any<Throwable>())} returns 0
-        every { Log.w(any(), any(), any())} returns 0
+        every { Log.v(any(), any()) } returns 0
+        every { Log.v(any(), any(), any()) } returns 0
+        every { Log.w(any(), any<String>()) } returns 0
+        every { Log.w(any(), any<Throwable>()) } returns 0
+        every { Log.w(any(), any(), any()) } returns 0
     }
 }
