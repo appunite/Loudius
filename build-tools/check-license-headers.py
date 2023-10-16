53559210ea56be622e782cd14db9395e96cef3d1
Jacek Marchwicki <jacek.marchwicki@gmail.com>
2023-10-16 17:05:08 +0200

    chore: added test for missing license header

---
+++ build-tools/check-license-headers.py
@@ -0,0 +1,56 @@
+import fnmatch
+import re
+import sys
+import typing
+import subprocess
+
+
+def get_tracked_files() -> typing.List[str]:
+    """Returns a list of all files tracked by Git."""
+
+    cmd = ["git", "ls-files"]
+    output = subprocess.check_output(cmd).decode("utf-8")
+    return output.splitlines()
+
+
+def check_license(file: str) -> bool:
+    with open(file, "r") as f:
+        contents = f.read()
+        return "Licensed under the Apache License" in contents
+
+
+matchers = [
+    re.compile(fnmatch.translate('*.kt')),
+    re.compile(fnmatch.translate('*.py')),
+]
+
+
+def should_check(file: str) -> bool:
+    for matcher in matchers:
+        if matcher.match(file):
+            return True
+    return False
+
+
+def main():
+    files = get_tracked_files()
+
+    errors: typing.List[str] = []
+    for file in files:
+        if should_check(file):
+            if not check_license(file):
+                errors.append(f"❌ File \"{file}\" does not contain the license phrase")
+        else:
+            print(f"ℹ️ Skipping check for \"{file}\"")
+
+    for error in errors:
+        print(error, file=sys.stderr)
+    if errors:
+        exit(1)
+    else:
+        print("✅ All files contain the license phrase")
+        exit(0)
+
+
+if __name__ == "__main__":
+    main()
