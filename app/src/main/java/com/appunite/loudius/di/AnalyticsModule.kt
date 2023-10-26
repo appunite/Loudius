3966243b7bf92437126a09df5a41e575cb830893
nowakweronika <nowakweronika1301@gmail.com>
2023-10-26 14:01:00 +0200

    hilt to koin

--- app/src/main/java/com/appunite/loudius/di/AnalyticsModule.kt
+++ app/src/main/java/com/appunite/loudius/di/AnalyticsModule.kt
@@ -1,29 +1,14 @@
 package com.appunite.loudius.di
 
-import android.content.Context
 import com.appunite.loudius.analytics.AnalyticsService
 import com.appunite.loudius.analytics.AnalyticsServiceImpl
 import com.google.firebase.analytics.FirebaseAnalytics
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
 
-@InstallIn(SingletonComponent::class)
-@Module
-class AnalyticsModule {
-
-    @Provides
-    @Singleton
-    fun provideAnalyticsService(
-        analytics: FirebaseAnalytics
-    ): AnalyticsService = AnalyticsServiceImpl(analytics)
-
-    @Provides
-    @Singleton
-    fun provideFirebaseAnalytics(
-        @ApplicationContext context: Context
-    ): FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
+val analyticsModule = module {
+    single<AnalyticsService> {
+        val firebaseAnalytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(androidContext())
+        AnalyticsServiceImpl(firebaseAnalytics)
+    }
 }
